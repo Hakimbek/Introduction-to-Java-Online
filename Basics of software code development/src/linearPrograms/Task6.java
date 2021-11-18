21361b4bea904b31e99e06f0d9202e54a26eb5c4
Hakimbek Bahramov <abduhakim.bahramv@gmail.com>
2021-11-18 20:07:56 +0500

    add text of task

--- Basics of software code development/src/linearPrograms/Task6.java
+++ Basics of software code development/src/linearPrograms/Task6.java
@@ -2,6 +2,10 @@ package linearPrograms;
 
 import java.util.Scanner;
 
+/**
+ * Для данной области составить линейную программу, которая печатает true если точка с координатами(x, y)
+ * принадлежит закрашенной области, и false - в противном случае
+ */
 public class Task6 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
